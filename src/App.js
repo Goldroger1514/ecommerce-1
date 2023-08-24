e9c63984e0e2c4d1159b33c238720533e45e43e4
Nadim <nadimrifaii3@gmail.com>
2023-08-24 12:14:26 +0300

    Fresh start

--- src/App.js
+++ src/App.js
@@ -6,21 +6,5 @@ let App = () => {
       <Route path='/' element={<Home />} />
     </Routes>
   )
-  /**
-   * We want to wrap anything that is going to be routable inside of this Routes component
-   * Essentially what Routes does is that it allows this application to register those route level components
-   * that will then in turn render a specific compnent when it matchees this specific rout that you're looking for
-   * ## 
-   * <Route path='/'/> , meaning because this is at the base URL level, when we just land on the URL, then i'm going to match
-   * So moment this matches , i want you to now render this element
-   * I want you to render specific components based on the URL
-   * The reason why these components are able to connect to the URL and therefore render the appropriate elements ,is because of the fact that these 
-   * are nested inside of the BrowserRouter component, because by doing this we're extending that ability to read from the URL and keep track of 
-   * navigation history and all those things and respond in turn with it
-   * Because those components , the routes and the root component rely on being wrapped in an outside BrowserRouter component
-   * 
-   * So our app component is really just the entry opint but all of those components that we leverage are now nested inside of BrowserRouter component
-   * That's why we want to wrap our application inside of BorwserRouter so we can leverage the underlying browser functionality when it comes to navigation
-   */
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,28 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-/**
- * BorwserRouter is a component
- * BorwserRouter is the generic router it leverages the URL in order to keep track of the history of where the user is navigating through and it behaves
- * as you typically would expect any kind of routing based on your URL to behave
- * There are other forms of routers inside of react-router-dom , but we won't really use them a lot
- * Now because we've wrapped our application component inside of our browser router component, we can access all of the different features that come in react-router-dom
- * specific to this browserRouter that is now going to control all of routing inside of our nested application
- */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  /**
-   * Right now App represents our entire application, however it renders the directory component directly
-   * We don't want that anymore 
-   * instead , now we want to render a home page and the directory kind of just lives on the home page
-   * ###
-   * routes is a folder that will hold all of our route top level components
-   * The components inside of the routes folder are related to routes, they're not these generic components that we're building that we can use in numerous places
-   * Essentially, these home components are these route level components are really only used for routing
-   */
-
 );
 reportWebVitals();
