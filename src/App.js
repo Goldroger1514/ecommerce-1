9f82f67e5dc09e950e1d5b67650a004b4d8ca077
Nadim <nadimrifaii3@gmail.com>
2023-08-24 11:30:04 +0300

    <Routes/> and <Route/>

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
-
-import Directory from './components/directory/directory.component'
+import Home from './routes/home/home.component'
+import { Routes, Route } from 'react-router-dom'
 let App = () => {
-  let categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png",
-      testing: 10
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
   return (
-    <Directory categories={categories} />
-  );
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  )
+  /**
+   * We want to wrap anything that is going to be routable inside of this Routes component
+   * Essentially what Routes does is that it allows this application to register those route level components
+   * that will then in turn render a specific compnent when it matchees this specific rout that you're looking for
+   * ## 
+   * <Route path='/'/> , meaning because this is at the base URL level, when we just land on the URL, then i'm going to match
+   * So moment this matches , i want you to now render this element
+   */
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -17,6 +17,15 @@ root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  /**
+   * Right now App represents our entire application, however it renders the directory component directly
+   * We don't want that anymore 
+   * instead , now we want to render a home page and the directory kind of just lives on the home page
+   * ###
+   * routes is a folder that will hold all of our route top level components
+   * The components inside of the routes folder are related to routes, they're not these generic components that we're building that we can use in numerous places
+   * Essentially, these home components are these route level components are really only used for routing
+   */
 
 );
 reportWebVitals();
