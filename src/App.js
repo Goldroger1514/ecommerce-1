241a30aed3b43e62a3814f0be715eef1ec142544
Nadim <nadimrifaii3@gmail.com>
2023-08-24 12:10:10 +0300

    Routes and route 2

--- src/App.js
+++ src/App.js
@@ -13,6 +13,14 @@ let App = () => {
    * ## 
    * <Route path='/'/> , meaning because this is at the base URL level, when we just land on the URL, then i'm going to match
    * So moment this matches , i want you to now render this element
+   * I want you to render specific components based on the URL
+   * The reason why these components are able to connect to the URL and therefore render the appropriate elements ,is because of the fact that these 
+   * are nested inside of the BrowserRouter component, because by doing this we're extending that ability to read from the URL and keep track of 
+   * navigation history and all those things and respond in turn with it
+   * Because those components , the routes and the root component rely on being wrapped in an outside BrowserRouter component
+   * 
+   * So our app component is really just the entry opint but all of those components that we leverage are now nested inside of BrowserRouter component
+   * That's why we want to wrap our application inside of BorwserRouter so we can leverage the underlying browser functionality when it comes to navigation
    */
 }
 export default App;
