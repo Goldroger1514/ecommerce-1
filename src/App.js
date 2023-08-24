df952f46ab29998215e0721a475fcc9e87c06dec
Nadim <nadimrifaii3@gmail.com>
2023-08-24 13:41:27 +0300

    finished nav-bar first version

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import Home from './routes/home/home.component'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Outlet } from 'react-router-dom'
 let App = () => {
+  let Navigation = () => {
+    return (
+      <div className='nav-bar' >
+        <div className='links'>
+          <h1>I am the navigation Bar</h1>
+        </div>
+        <Outlet />
+      </div>
+    )
+    /**
+     * This is our top level component 
+     * The outlet now in turn , render everything else inside
+     * Inside the <Outlet/> , this is what we'll swap based on the remaining route matching inside and this pattern matching 
+     * This deeper route matching you can do continually , however nested deep you go
+     */
+  }
   let Shop = () => {
     return <h1>Hello i'm the shop</h1>
   }
   return (
     <Routes>
-      <Route path='/home' element={<Home />} >
-        <Route path='/home/shop' element={<Shop />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index={true} element={<Home />} />
+        {
+          /**
+           * When you match just / with nothing on it , then the Home component should be renderd with it
+           * The home component is the base component inside of the <Outlet/>
+           */
+        }
+        <Route path='/shop' element={<Shop />} />
       </Route>
     </Routes>
   )
