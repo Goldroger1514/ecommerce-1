eed353016d2ec9c03d925636d993109224a99b69
Nadim <nadimrifaii3@gmail.com>
2023-08-26 11:40:33 +0300

    signIn with redirect

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
-// Your web app's Firebase configuration
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth'
 const firebaseConfig = {
     xx: 1,
     apiKey: "AIzaSyBPMcx8TlZryYLVn-dZBXyGSl9LaQv9L1c",
@@ -14,51 +13,27 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firbaseApp = initializeApp(firebaseConfig);
-let provider = new GoogleAuthProvider()//gives back provider instance
-provider.setCustomParameters({
-    prompt: 'select_account',//what this means that everytime someone interacts with our provider, we want to awlays force them to select an account
+let googleProvider = new GoogleAuthProvider()
+googleProvider.setCustomParameters({
+    prompt: 'select_account',
 })
 export let auth = getAuth()
 export let signInWithGooglePopup = () => {
-    return signInWithPopup(auth, provider)
+    return signInWithPopup(auth, googleProvider)
 }
-//Starting users doc
-let db = getFirestore()
-// This allows us now to tell Firebase when we want to get a document or we want set a document or anything ike that related to our database
-// It directly points to our database
+export let signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
 /**
- * doc: What we need to actually get a document instance
- * getDoc: When you want to access the data on those document
- * setDoc: When you want to set the data on those document
+ * There are different providers that we can have 
+ * wether we want to sign in with facebook , or github ...
+ * These are all different providers that we can pull in
+ * This is why these providers are instantiated as classes
+ * with signInWithGooglePopup , i know that this pop up is going to be google because the provider i'm generating for it is Google provider
  */
+let db = getFirestore()
 export let createUserDocumentFromAuth = async (userAuth) => {
-    /**
-     * What we need to do first is that we need if there is an existing document reference
-     */
-    let userDocRef = doc(db, 'users', userAuth.uid)//db,collectionName,(id|key)
-    /**
-     * What's interesting about Google Firestore is that even though right now this object doesn't exist (the document reference doesn't exist in our database)
-     * But Google will still generate this object
-     * The reason why Google does this is because this reference points to some now unique point inside of the database
-     * There's nothing there for this ID So google say there's no harm i'm not going to override anything with this ID
-     * By giving us this reference Google wants us to suer this specific document reference object that they provided us in order to set data there
-     * because it's already king of pointing to some place inside of our database
-     * We can imagine that there is now this users collection that Google hasn't made yet but it knows that 's it pointing there
-     * And inside of that users's collection is that specific user ID
-     */
+    let userDocRef = doc(db, 'users', userAuth.uid)
     console.log(userDocRef)
     let userSnapshot = await getDoc(userDocRef)
-    /**
-     * The snapShot allows us to check wether or not there is an instance of it that exists inside of our database
-     * and it also allows us to access the data
-     */
-    console.log(userSnapshot)
-    console.log(userSnapshot.exists())//false
-    /**
-     * exists() methods tells us if inside of our database does that reference object and the data related to that reference exists or not
-     */
-    // if users data exists
-    // return userDocRef
     if (!userSnapshot.exists()) {
         let { displayName, email } = userAuth
         let createdAt = new Date()
@@ -74,7 +49,5 @@ export let createUserDocumentFromAuth = async (userAuth) => {
     } else {
         return userDocRef
     }
-    // if user data doesn't exist
-
 }
-//Start sign in with redirect
+
