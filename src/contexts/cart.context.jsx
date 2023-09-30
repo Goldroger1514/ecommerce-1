b708be0afddfc96c02494e840eade2af1c2db054
Nadim <nadimrifaii3@gmail.com>
2023-09-30 21:25:57 +0300

    contextVSredux

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,21 +1,10 @@
 import { createContext, useState, useEffect, useReducer } from "react";
 import { createAction } from "../utils/reducer/reduer.utils";
 let addCartItem = (cartItems, productToAdd) => {
-  //find if cartItems contains product to add
   let exists = cartItems.find(item => item.id == productToAdd.id)
-  //if found increment quantity
   if (exists) {
     return cartItems.map(item => item.id == productToAdd.id ? { ...item, quantity: item.quantity + 1 } : item)
-    let newArr = []
-    for (let item of cartItems)
-      if (item.id == productToAdd.id) {
-        newArr.push({ ...item, quantity: item.quantity + 1 })
-      } else {
-        newArr.push({ ...item })
-      }
-    return newArr
   }
-  //return new array with modified cartItems
   return [...cartItems, { ...productToAdd, quantity: 1 }]
 }
 let incDec = (cartItems, product, op) => {
@@ -40,41 +29,16 @@ export let CartContext = createContext({
 let USER_ACTION_TYPES = {
   SET_IS_CART_OPEN: 'SET_IS_CART_OPEN',
   SET_CART_ITEMS: 'SET_CART_ITEMS',
-  // SET_CART_COUNT: 'SET_CART_COUNT',
-  // SET_CART_TOTAL: 'SET_CART_TOTAL'
 }
 let INITIAL_STATE = {
   isCartOpen: false,
   cartItems: [],
   cartCount: 0,
   total: 0
-  /**
-   * Our reducer only store readable values
-   * The best way to think about our final output is with the INITIAL_STATE object and it's gonna include all the readable value of the context
-   * The INITIAL_STATE object gives us the object that we need to keep track of when it comes to what our actual reducer should return
-   */
 }
-//Shape of the final output , include all the readable value of our context
 let cartReducer = (currentState, action) => {
   let { type, payload } = action
-  /**
-   * Once we have the type and ppayload , now we can determine with these two values from the action what do we need to return as the UPDATED_STATE
-  */
-  // let newCartItems=addCartItem(currentState.cartItems,payload)
-  // let newCartTotal=newCartItems.reduce()
-
   switch (type) {
-    // case USER_ACTION_TYPES.SET_CART_ITEMS:
-    //   return {
-    //     ...currentState,
-    //     cartItems:addCartItem(currentState.cartItems,payload),
-    //     cartCount:currentState.cartCount+1,
-    //     cartTotal:1
-    //   }
-    //   break;
-    /**
-     * All a reducer should care about is just updating the state, it shouldn't worry about anything regarding how to update that state 
-     */
     case USER_ACTION_TYPES.SET_CART_ITEMS:
       return {
         ...currentState,
@@ -91,15 +55,6 @@ let cartReducer = (currentState, action) => {
       break
   }
 }
-/**
- * Our reducer should not handle any business logic, meaning that inside of a reducer
- * if we were to receive some item, some of we might be thinking 
- * Let's just create actions
- * for example:
- */
-// let addToCartAction = (itemToAdd) => {
-//   dispatch({ type: "ADD_TO_CART", payload: itemToAdd })
-// }
 let remove = (cartItems, product) => {
   let index = 0;
   for (let item of cartItems)
@@ -112,10 +67,6 @@ let remove = (cartItems, product) => {
 }
 
 export let CartProvider = ({ children }) => {
-  // let [isCartOpen, setIsCartOpen] = useState(false)
-  // let [cartItems, setCartItems] = useState([])
-  // let [cartCount, setCartCount] = useState(0)
-  // let [total, setTotal] = useState(0)
   let [state, dispatchFunction] = useReducer(cartReducer, INITIAL_STATE)
   let { cartItems, cartCount, total, isCartOpen } = state
   let updateCartReducer = (newCartItems) => {
@@ -125,19 +76,7 @@ export let CartProvider = ({ children }) => {
     let newCount = cartItems.reduce((acc, element) => {
       return acc + element.quantity
     }, 0)
-    // dispatchFunction({ type: USER_ACTION_TYPES.SET_CART_ITEMS, payload: { cartItems: newCartItems, cartCount: newCount, total: total } })
     dispatchFunction(createAction(USER_ACTION_TYPES.SET_CART_ITEMS, { cartItems: newCartItems, cartCount: newCount, total: total }))
-    /**
-     * generate newCartTotal
-     * 
-     * generate newCartCount
-     * 
-     * dispatch new action with payload that looks like {
-     * newCartItems,
-     * newCartTotal,
-     * newCartCount
-     * }
-     */
   }
   let setIsCartOpen = () => {
     dispatchFunction({ type: USER_ACTION_TYPES.SET_IS_CART_OPEN, payload: isCartOpen })
@@ -154,24 +93,6 @@ export let CartProvider = ({ children }) => {
     let newCartItems = [...remove(cartItems, product)]
     updateCartReducer(newCartItems)
   }
-  // let calculateTotal = (cartItems) => {
-  //   let total = 0
-  //   for (let item of cartItems)
-  //     total += item.quantity * item.price
-  //   setTotal(total)
-  // }
-  // useEffect(() => {
-  //   let newCount = cartItems.reduce((acc, element) => {
-  //     return acc + element.quantity
-  //   }, 0)
-  //   setCartCount(newCount)
-  // }, [cartItems])
-  // useEffect(() => {
-  //   calculateTotal(cartItems)
-  // }, [cartItems])
   let value = { isCartOpen, total, increaseDecrease, setIsCartOpen, removeItem, addItemToCart, cartItems, cartCount }
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
-/**
- * the good time to use reducer is when one update needs to modify multiple readable values inside of our state 
- */
--- src/contexts/user-context.component.jsx
+++ src/contexts/user-context.component.jsx
@@ -8,12 +8,6 @@ export let USER_ACTION_TYPES = {
   'SET_CURRENT_USER': 'SET_CURRENT_USER'
 }
 let userReducer = (currentState, action) => {
-  /**
- * In this function, currentState is the current state of your user-related context data, which is provided 
- * by the useReducer hook when you call it with the userReducer function.
- *  The useReducer hook manages the state and passes the current state as the first argument to your reducer function 
- * (userReducer in this case) whenever an action is dispatched.
- */
   console.log('Dispatched')
   console.log(action)
   let { type, payload } = action
@@ -36,44 +30,13 @@ let userReducer = (currentState, action) => {
 }
 let INITIAL_STATE = {
   currentUser: null
-  /**
-   * Initially, when you create the state using useReducer:
-    let [state, dispathFunction] = useReducer(userReducer, INITIAL_STATE)
-    state is initialized with the INITIAL_STATE object. This is the initial state of your context.
-   */
 }
 export let UserProvider = ({ children }) => {
-  // let [currentUser, setCurrentUser] = useState(null)
   let [state, dispatch] = useReducer(userReducer, INITIAL_STATE)//state object, dispatch function
   let { currentUser } = state
   let setCurrentUser = (user) => {
-    // dispathFunction({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user })
     dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user))
-    /**
-     * When you dispatch an action using dispathFunction:
-     * dispathFunction({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user })
-     * The useReducer hook takes the current state (state), passes it to your userReducer function as currentState,
-     *  along with the action you dispatched ({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user }),
-     *  and then calls your reducer function (userReducer) with these arguments.
-     */
   }
-  /**
-   * dispatch function whenever you call it , you pass it an action object
-   * So if we want this useReducer to receive an action , we call dispatch and dispatch will take that action and then pass it in to userReducer
-   */
-  console.log(currentUser)
   let value = { currentUser, setCurrentUser }
   return <UserContext.Provider value={value} >{children}</UserContext.Provider>
 }
-/**
- * Reducers as code are very simple , they're pretty much a function that returns an object
- * let userReducer=(currentState,action)=>{
- * return{
- *  currentUser:null
- * }
- * }
- * - Reducers change the object that we get back and the properties and the values inside them based on the action
- */
-/**
- * 
- */
